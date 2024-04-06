1c545643f0c4076445dcb0a9789daf717e545509
PavlencoIgorAlex <pavlencoigoralex@gmail.com>
2024-04-06 09:12:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradesViewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
